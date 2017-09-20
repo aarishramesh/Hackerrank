da72a8e595f2c8490e249ea1a90850fd15ab6933
aarishramesh <aarish.ramesh@gmail.com>
2017-09-21 01:17:56 +0530

    Finding factorial using BigInteger

---
+++ src/tutorials/Algorithms/FindingFactorial.java
@@ -0,0 +1,18 @@
+package tutorials.Algorithms;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class FindingFactorial {
+	  public static void main(String[] args) {
+	        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+	        Scanner sc = new Scanner(System.in);
+	        long n = sc.nextInt();
+	        BigInteger factorial = new BigInteger("1");
+	        while (n > 0) {
+	            factorial = factorial.multiply(BigInteger.valueOf(n));
+	            n = n -1;
+	        }
+	        System.out.println(factorial);
+	    }
+}
